4644f4bdd1cda008837b9e2082eee949a2e57267
Evan Chrisinger <echrisinger@gmail.com>
2020-04-12 17:00:15 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfswatch',
+    version='0.1',
+    description='Python bindings for libfswatch.',
+    packages=find_packages(),
+)
